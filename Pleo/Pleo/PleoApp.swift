afd77a21b6cc2ea05bcf4b9e5819cdc482a6d7d3
Vinter Marco <vintermarco98@gmail.com>
2023-12-16 16:22:49 +0200

    first commit

---
+++ Pleo/Pleo/PleoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PleoApp.swift
+//  Pleo
+//
+//  Created by Vinter Marco on 16.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct PleoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
